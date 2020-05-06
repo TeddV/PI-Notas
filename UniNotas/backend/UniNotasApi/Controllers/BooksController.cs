f6e9f564d49158ab50e90c7ef401d64523d68a2b
TeddV <vinicius.b37@outlook.com>
2020-05-06 17:10:34 -0300

    Controllers genericos

---
+++ UniNotas/backend/UniNotasApi/Controllers/BooksController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniNotasApi.Data;
+using UniNotasApi.Models;
+
+namespace UniNotasApi.Controllers
+{
+    [Route("Book")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly UniNotasContext _context;
+
+        public BooksController(UniNotasContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<IEnumerable<Book>>> Getbooks()
+        {
+            return await _context.books.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        [Route("{id:long}")]
+        public async Task<ActionResult<Book>> GetBook(long id)
+        {
+            var book = await _context.books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPut("{id}")]
+        [Route("{id:long}")]
+        public async Task<IActionResult> PutBook(long id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+        [HttpDelete("{id}")]
+        [Route("{id:long}")]
+        public async Task<ActionResult<Book>> DeleteBook(long id)
+        {
+            var book = await _context.books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return book;
+        }
+
+        private bool BookExists(long id)
+        {
+            return _context.books.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ UniNotas/backend/UniNotasApi/Controllers/NotesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniNotasApi.Data;
+using UniNotasApi.Models;
+
+namespace UniNotasApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly UniNotasContext _context;
+
+        public NotesController(UniNotasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Notes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Note>>> Getnotes()
+        {
+            return await _context.notes.ToListAsync();
+        }
+
+        // GET: api/Notes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> GetNote(long id)
+        {
+            var note = await _context.notes.FindAsync(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return note;
+        }
+
+        // PUT: api/Notes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNote(long id, Note note)
+        {
+            if (id != note.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(note).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Notes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Note>> PostNote(Note note)
+        {
+            _context.notes.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNote", new { id = note.Id }, note);
+        }
+
+        // DELETE: api/Notes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Note>> DeleteNote(long id)
+        {
+            var note = await _context.notes.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            _context.notes.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return note;
+        }
+
+        private bool NoteExists(long id)
+        {
+            return _context.notes.Any(e => e.Id == id);
+        }
+    }
+}
